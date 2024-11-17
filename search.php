9344d7aedd48347f2e17bd2b41f2a4951adafe47
RichardLoz <lozanorichard32@gmail.com>
2024-11-17 10:11:43 -0300

    Estructura del proyecto Tigre_Canino

---
+++ search.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Search results</title>
+<meta charset="utf-8">
+<meta name = "format-detection" content = "telephone=no" />
+<link rel="icon" href="img/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+<meta name="description" content="Your description">
+<meta name="keywords" content="Your keywords">
+<meta name="author" content="Your name">   
+<meta name = "format-detection" content = "telephone=no" /> 
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<!--CSS-->
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/responsive.css">
+<link rel="stylesheet" href="css/style.css">
+<!--JS-->
+<script src="js/jquery.js"></script>
+<script src="js/jquery-migrate-1.1.1.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="search/search.js"></script>
+<script src="js/superfish.js"></script>
+<script src="js/jquery.mobilemenu.js"></script>
+<script src="js/jquery.ui.totop.js"></script> 
+<script src="js/jquery.equalheights.js"></script>
+<!--[if lt IE 8]>
+    <div style='text-align:center'><a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://www.theie6countdown.com/images/upgrade.jpg"border="0"alt=""/></a></div>  
+<![endif]-->
+<!--[if lt IE 9]>
+  <script src="js/html5shiv.js"></script>
+<![endif]-->
+</head>
+<body>
+<div class="global">
+<!--header-->
+<header>
+    <div class="upbox">
+        <div class="container">
+            <div class="row">
+                <article class="span12">
+                    <p>You have not yet registered to our club? <a href="#">Login</a> or <a href="#" class="white">Register</a></p>
+                </article>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <article class="span12">
+                <h1 class="brand"><a href="index.html"><img src="img/logo.png" alt=""></a></h1>
+                <div class="navbar navbar_ clearfix">
+                    <div class="navbar-inner">      
+                        <div class="clearfix">
+                            <div class="nav-collapse nav-collapse_">
+                                <ul class="nav sf-menu clearfix">
+                                    <li><a href="index.html">Home<em></em><strong></strong></a></li> 
+                                    <li class="sub-menu"><a href="index-1.html">About<span></span><em></em><strong></strong></a>
+                                        <ul class="submenu">
+                                            <li><a href="#">Excepteur sint occaecat</a></li>
+                                            <li><a href="#">Cupidatat non</a></li>
+                                            <li><a href="#">Proident sunt in culpa</a></li>
+                                        </ul>
+                                    </li> 
+                                    <li><a href="index-2.html">Supplies<em></em><strong></strong></a></li>
+                                    <li><a href="index-3.html">Our Staff<em></em><strong></strong></a></li>
+                                    <li><a href="index-4.html">Contacts<em></em><strong></strong></a></li>                  
+                                </ul>
+                            </div>                                                                                                                
+                        </div>
+                    </div>  
+                </div>
+            </article>
+        </div>
+    </div>
+</header>
+<!--content-->
+<div class="container padTop padBot">
+    <div class="row">
+        <article class="span12">
+             <h2>Search result:</h2>
+            <div id="search-results" class="left-align"></div>
+        </article>
+    </div>
+</div>
+<section class="follow-box">
+    <div class="container">
+        <div class="row">
+            <article class="span12">
+                <h2>Follow us</h2>
+                <div class="row">
+                    <ul class="follow">
+                        <li class="span4">
+                            <figure><img src="img/follow_icon1.png" alt=""></figure>
+                            <a href="#">Facebook</a>
+                            <p>Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempo.</p>
+                        </li>
+                        <li class="span4">
+                            <figure><img src="img/follow_icon2.png" alt=""></figure>
+                            <a href="#">Twitter</a>
+                            <p>Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempo.</p>
+                        </li>
+                        <li class="span4">
+                            <figure><img src="img/follow_icon3.png" alt=""></figure>
+                            <a href="#">You Tube</a>
+                            <p>Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempo.</p>
+                        </li>
+                    </ul>
+                </div>
+            </article>
+        </div>
+    </div>
+</section>
+</div>
+<!--footer-->
+<footer>
+    <div class="main">
+        <div class="container">
+            <div class="row">
+                <article class="span12">
+                    <p>Dog Club &copy; 2013. <a href="index-5.html">Privacy Policy</a></p>
+                </article>
+            </div>
+        </div> 
+    </div>  
+</footer>
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
